bce96abc48875c76923e296b9e9629f2926486b9
Soutick Samanta <soutick1505@gmail.com>
2025-12-06 01:25:20 +0530

    update class

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    private String id;
+    private String name;
+
+    Student(String id, String name){
+        this.id =id;
+        this.name=name;
+    }
+}
+
--- aaa.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class aaa {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-
-        }
-    }
-
